5914d651f4d900dec7775d4dc7d6ae47761ba1e7
Михаил <mmamchenkov@fil-it.ru>
2022-04-25 19:41:29 +0300

    Initial commit

---
+++ src/test/java/ProdMonitoringApiTest.java
@@ -0,0 +1,28 @@
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
+@Feature(value = "Прод - Мониторинг")
+@Story("Проверка доступности сайтов аптек 366 и Горздрав")
+@DisplayName("Проверка доступности сайтов аптек 366 и Горздрав")
+public class ProdMonitoringApiTest {
+
+    @DisplayName("Мониторинг доступности сайта https://366.ru/")
+    @Test
+    public void checkSite366() {
+        given().
+                when().get("https://366.ru/").
+                then().statusCode(200);
+    }
+
+    @DisplayName("Мониторинг доступности сайта https://gorzdrav.org/")
+    @Test
+    public void checkSiteGz() {
+        given().
+                when().get("https://gorzdrav.org/").
+                then().statusCode(200);
+    }
+}
