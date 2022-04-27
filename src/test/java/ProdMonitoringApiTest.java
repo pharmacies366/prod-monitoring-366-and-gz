fe4cf50fd6ea0f0824831f35dc1516417edee898
Михаил <mmamchenkov@fil-it.ru>
2022-04-27 13:46:36 +0300

    test commit

--- src/test/java/ProdMonitoringApiTest.java
+++ src/test/java/ProdMonitoringApiTest.java
@@ -1,28 +1,31 @@
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
-
-import static io.restassured.RestAssured.*;
 
 @Feature(value = "Прод - Мониторинг")
 @Story("Проверка доступности сайтов аптек 366 и Горздрав")
 @DisplayName("Проверка доступности сайтов аптек 366 и Горздрав")
 public class ProdMonitoringApiTest {
 
-    @DisplayName("Мониторинг доступности сайта https://366.ru/")
+/*    @DisplayName("Мониторинг доступности сайта https://366.ru/")
     @Test
     public void checkSite366() {
         given().
                 when().get("https://366.ru/").
                 then().statusCode(200);
     }
+    */
 
-    @DisplayName("Мониторинг доступности сайта https://gorzdrav.org/")
+    @DisplayName("Мониторинг доступности сайта https://366.ru/")
     @Test
-    public void checkSiteGz() {
-        given().
-                when().get("https://gorzdrav.org/").
-                then().statusCode(200);
+    public void checkSite366() {
+        Response response = RestAssured.get("https://366.ru/");
+        System.out.println(response.getStatusCode());
+        int statusCode = response.statusCode();
+        Assert.assertEquals(statusCode, 200);
     }
 }
