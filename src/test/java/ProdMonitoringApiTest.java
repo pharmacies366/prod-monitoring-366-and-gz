10c9eb6e85066bdc8956876e0e24515a8b8d6edf
Михаил <mmamchenkov@fil-it.ru>
2022-06-09 12:49:46 +0300

    test commit

--- src/test/java/ProdMonitoringApiTest.java
+++ src/test/java/ProdMonitoringApiTest.java
@@ -1,10 +1,15 @@
 
+import io.qameta.allure.Allure;
 import io.qameta.allure.Story;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Story("Мониторинг")
 @DisplayName("Проверка доступности сайта аптек 366")
@@ -26,5 +31,21 @@ public class ProdMonitoringApiTest {
         System.out.println(response.getStatusCode());
         int statusCode = response.statusCode();
         Assert.assertEquals(statusCode, 200);
+    }
+
+    public void addAllureLog()  {
+        String fileName = "Путь к лог файлу для Allure";
+        String content = null;
+        try {
+            content = Files.lines(Paths.get(fileName)).reduce("",(a, b) -> a + "\n" + b);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] split = content.split("Запущен тест: ");
+        int i = split.length - 1;
+        String s = split[i];
+        Allure.getLifecycle().updateTestCase((t) -> {
+            t.setStatusDetails( t.getStatusDetails().setMessage(s));
+        });
     }
 }
